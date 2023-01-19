d7fcc637395f234d073d9bd23d2e0d6df3ac8b24
Konstantin Bläsi <kblaesi@gmail.com>
2023-01-19 23:42:14 +0100

    Render child relying on context by providing the fake value directly, no extra testing parent involved

--- src/tests/component/child-context.test.ts
+++ src/tests/component/child-context.test.ts
@@ -1,16 +1,37 @@
 import {render} from '@testing-library/svelte'
 import {describe, expect, it} from 'vitest';
 import FakeApp from "$lib/testing/FakeApp.svelte";
+import Child from "$lib/context/Child.svelte";
+import type {Data} from "$lib/context/context";
+import {key} from "../../lib/context/context";
 
 describe('Test Child.svelte', () => {
 
     it('Renders with faked context', () => {
-        const { container } = render(FakeApp)
+        const {container} = render(FakeApp)
 
-        const headline  = container.getElementsByTagName("h2")[0].textContent
+        const headline = container.getElementsByTagName("h2")[0].textContent
         // FakeParent.svelte provides this context
         expect(headline).toContain("Welcome to Paris")
         // Test should not receive the real Parent.svelte's context
         expect(headline).not.toContain("Welcome to Amsterdam")
     });
+
+    it("Renders directly", () => {
+        // context value
+        const data = {
+            city: "New York"
+        } satisfies Data
+
+        const host = document.createElement('div');
+        new Child({
+            context: new Map([
+                [key, data]]
+            ),
+            target: host
+        })
+
+        const headline = host.getElementsByTagName("h2")[0].textContent
+        expect(headline).toContain(`Welcome to ${data.city}`)
+    })
 });
